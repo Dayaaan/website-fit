f28de7a62046f57b693a065e8ca4ab6b831301d2
dayaaan <dayaaan.vu@gmail.com>
2018-12-17 10:50:34 +0100

    first commit

--- application/config/database.php
+++ application/config/database.php
@@ -5,9 +5,9 @@
  */
 
 
-$config['dsn']      = 'mysql:host=localhost;dbname=coachtessier';
-$config['password'] = 'root';
-$config['user']     = 'root';
+$config['dsn']      = 'mysql:host=vumartingg061289.mysql.db;dbname=vumartingg061289';
+$config['password'] = 'Vumartin061289';
+$config['user']     = 'vumartingg061289';
 
 
 // $config['dsn']      = 'mysql:host=vumartingg061289.mysql.db;dbname=vumartingg061289';
